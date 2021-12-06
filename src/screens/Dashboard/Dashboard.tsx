8ad3d0f097e0032e8c95d79e4cce96e170c06628
Daniel Waite <danielwaite6@hotmail.com>
2021-12-05 23:19:52 -0300

    first commit

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import {
     Container,
@@ -9,21 +8,22 @@ import {
     UserInfo,
     UserGreeting,
     UserName,
+    UserWrapper
 } from './styles';
 
 export function Dashboard() {
     return (
         <Container>
             <Header>
-
-                <UserInfo>
-                    <Photo source={{ uri: 'https://upload.wikimedia.org/wikipedia/pt/f/fd/Tom_and_Jerry.png' }} />
-                    <User>
-                        <UserGreeting>Olá, </UserGreeting>
-                        <UserName>Daniel, </UserName>
-                    </User>
-                </UserInfo>
-
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://upload.wikimedia.org/wikipedia/pt/f/fd/Tom_and_Jerry.png' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Daniel, </UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
             </Header>
         </Container>
     );
